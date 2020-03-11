b0c232f469403139e752a1b27c692102da94e318
Oyewole Oni <ooni@eminenttechnology.com>
2020-03-11 11:26:43 +0100

    Advance component usage

---
+++ Vue-Practise/component-advance/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent ='appQuote'">Quote</button>
+                <button @click="selectedComponent ='appAuthor'">Author</button>
+                <button @click="selectedComponent ='appNew'">New</button>
+                <hr>
+                {{selectedComponent}}
+                <keep-alive>
+                    <component :is="selectedComponent">
+                    <p>Default content</p>
+                </component>
+                </keep-alive>
+
+               
+               <!-- <app-quote>
+                   <h1 slot="title">{{quoteTitle}}</h1>
+                   <p slot="content">A wonderful quote</p>
+               </app-quote> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue';
+    import Author from './components/Author.vue';
+    import New from './components/New.vue';
+    export default {
+          data: function(){
+        return {
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote'
+        }
+    },
+        components: {
+            appQuote: Quote,
+            appAuthor: Author,
+            appNew: New
+        }
+    }
+</script>
+
+<style>
+</style>

8152371474ab788291297e3af7f8ea5e250e5ccc
Oyewole Oni <ooni@eminenttechnology.com>
2020-03-13 14:43:04 +0100

    filters and Mixins

---
+++ Vue-Practise/filter-mixins/src/list.vue
@@ -0,0 +1,23 @@
+<template>
+        <div>
+            <input type="text" v-model="filterText">
+            <ul>
+                <li v-for="fruit in filteredFruits" :key="fruit">{{fruit}}</li>
+            </ul>
+        </div>
+    
+</template>
+
+<script>
+    import { fruitMixin } from './FruitMixin.js'
+    export default {
+        mixins: [fruitMixin],
+        created() {
+            console.log('Inside List Created Hook')
+        }
+    }
+</script>
+
+<style>
+
+</style>

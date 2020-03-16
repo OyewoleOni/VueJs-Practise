1e5f3cccee336ddd7c8e247e9916b57077d35a78
Oyewole Oni <ooni@eminenttechnology.com>
2020-03-17 00:15:34 +0100

    Create Implementation

--- Vue-Practise/vue-test/src/views/Create.vue
+++ Vue-Practise/vue-test/src/views/Create.vue
@@ -4,7 +4,7 @@
          <div class="container">
         <form>
             <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6">
                     <div class="form-group">
                         <label for="">Equipment Name</label>
                         <input
@@ -24,19 +24,23 @@
                            
                     </div>
                     <div class="form-group">
-                        <label for="age">Equipment Status</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control"
-                                 v-model="equipment.equipmentStatus">
+                        <label for="age">Equipment Type</label>
+                          <validation-provider rules="required" v-slot="{ errors }">
+                            <select name="types" class="form-control" v-model="equipment.equipmentTypeName" :class="{'error-message': errors[0]}">
+                                <option v-for="(type, i) in types" :key="i" :selected="type.selected">{{type.name}}</option>
+                            </select>
+                        <span class="error-text">{{ errors[0] }}</span>
+                        </validation-provider>
                     </div>
 
                 </div>
             </div>
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary" @click.prevent="submitted">Submit!
+                            class="btn btn-primary" @click.prevent="postEquipment()">Submit!
+                    </button>
+                    <button
+                            class="btn btn-danger ml-2" @click="cancel">Cancel!
                     </button>
                 </div>
         </form>
@@ -46,6 +50,8 @@
 </template>
 
 <script>
+import { equipmentEventBus } from '../services/equipment'
+ import { eventBus } from './../main'
 export default {
      data () {
     return {
@@ -58,6 +64,7 @@ export default {
         equipmentStatusName: null,
         equipmentTypeId: null
       },
+      res: '',
       types: [
         {
           name: 'Indoor',
@@ -68,7 +75,83 @@ export default {
           value: 1
         }
       ],
-      isLoading: false
+      isLoading: false,
+    }
+  },
+  created () {
+    if (this.$route.params.id) {
+      // this.isLoading = true
+      equipmentEventBus.getEquipment(this.$route.params.id)
+        .then(response => response.json())
+        .then(json => {
+          
+          console.log(json);
+          this.equipment.equipmentName = json.name
+          this.equipment.equipmentQuantity = json.quantity
+          this.equipment.equipmentType = json.type
+          this.equipment.equipmentTypeName = this.equipment.equipmentType === 0 ? 'Indoor' : 'Outdoor'
+          this.equipment.equipmentStatus = json.status
+          this.equipment.equipmentStatusName = json.statusName
+          this.isLoading = false
+        })
+        .catch(err => {
+          if (err.types === 404) {
+            this.isLoading = false
+          }
+        })
+    }
+  }, 
+  methods: {
+    cancel() {
+      this.$router.push('/')
+    },
+    postEquipment(){
+      this.isLoading = true
+      this.equipment.equipmentTypeName === 'Indoor' ? this.equipment.equipmentType = 0 : this.equipment.equipmentType = 1
+      console.log(this.equipment)
+      let payload = []
+      if (this.$route.params.id) {
+        payload = {
+          id: this.$route.params.id,
+          name: this.equipment.equipmentName,
+          status: 0,
+          statusName: 'Created',
+          type: this.equipment.equipmentType,
+          quantity: +this.equipment.equipmentQuantity,
+          typeName: this.equipment.equipmentTypeName
+        }
+        equipmentEventBus.editEquipment(this.$route.params.id, payload)
+          .then(res => {
+            eventBus.$emit('alert', 'succesful')
+            this.$router.push('/')
+            this.isLoading = true
+            this.res = res
+          })
+          .catch(err => {
+            eventBus.$emit('alert', err)
+            // this.isLoading = true
+          })
+      } else {
+        payload = {
+          name: this.equipment.equipmentName,
+          status: 0,
+          statusName: 'Created',
+          type: this.equipment.equipmentType,
+          quantity: +this.equipment.equipmentQuantity,
+          typeName: this.equipment.equipmentTypeName
+        }
+        equipmentEventBus.createEquipment(payload)
+          .then(res => {
+            eventBus.$emit('alert', 'succesful')
+            this.$router.push('/')
+            this.isLoading = true
+            this.res = res
+          })
+          .catch(err => {
+            eventBus.$emit('alert', err)
+            // this.isLoading = true
+          })
+      }
     }
   }
 }
